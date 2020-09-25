8fb9bfa053c0efbf2bcde8bd019b7e0bd43db93f
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-09-25 14:18:13 +1200

    Saves output to txt file

--- timing.py
+++ timing.py
@@ -10,6 +10,12 @@ import numpy
 import time
 
 
+def print_and_write(text):
+    print(text)
+    with open("hill climbing data 100 samples.txt", "a") as file:
+        file.write(text + "\n")
+
+
 def time_function(problem, function):
     problem2 = NQueensProblem(state=problem.random_state())
     start = time.time()
@@ -35,20 +41,20 @@ def average_std(problem, function, number):
     expanded_standard_deviation = numpy.std(num_exanded_nodes)
     expanded_average = numpy.average(num_exanded_nodes)
     solving_probability = solved / number
-    print("expanded: {0:.5f} +- {1:.5f}".format(expanded_average,
-                                                expanded_standard_deviation))
-    print("time: {0:.5f} +- {1:.5f}".format(time_average,
-                                            time_standard_deviation))
-    print("solving_probability: ", solving_probability)
+    print_and_write("expanded: {0:.5f} +- {1:.5f}".format(expanded_average,
+                                                          expanded_standard_deviation))
+    print_and_write("time: {0:.5f} +- {1:.5f}".format(time_average,
+                                                      time_standard_deviation))
+    print_and_write("solving_probability: " + str(solving_probability))
 
 
-NUM_TRIALS = 1000
-NUM_QUEENS = 15
+NUM_TRIALS = 100
+NUM_QUEENS = 50
 
-for i in range(NUM_QUEENS, NUM_QUEENS + 3):
-    print("N:", i)
+for i in range(1, NUM_QUEENS + 5):
+    print_and_write("N: " + str(i))
     problem = NQueensProblem(N=i)
     average_std(problem, hill_climbing_instrumented, NUM_TRIALS)
     average_std(problem, hill_climbing_sideways, NUM_TRIALS)
     average_std(problem, hill_climbing_random_restart, NUM_TRIALS)
-    print()
+    print_and_write("")
